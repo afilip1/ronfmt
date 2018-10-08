f86c885764d6241b6c9a87dde3ef457ff760bc0b
Anton Filippov <a.filippov@protonmail.com>
2018-10-08 13:33:48 +0700

    Fix errors in grammar, add primitive extensions formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use pest_derive::*;
-
 use pest::Parser;
+use pest_derive::*;
 
 #[derive(Parser)]
 #[grammar = "ron.pest"]
 struct RonParser;
 
 fn main() {
-    let file = std::fs::read_to_string("test.ron").unwrap();
-    let ron = RonParser::parse(Rule::ron_file, &file);
-    println!("{:#?}", ron);
+    let file = std::fs::read_to_string("test.ron").expect("unable to read file");
+    let ron = RonParser::parse(Rule::ron_file, &file)
+        .expect("unable to parse RON")
+        .next()
+        .unwrap();
+
+    let mut extensions = vec![];
+
+    for item in ron.into_inner() {
+        match item.as_rule() {
+            Rule::extension => {
+                for ext_name in item.into_inner() {
+                    extensions.push(ext_name.as_str());
+                }
+            }
+            Rule::value => {}
+            Rule::EOI => {}
+            _ => unreachable!(),
+        }
+    }
+
+    println!("#![enable({})]", extensions.join(", "));
 }
