67ce981ae64a1d6cea33d4075c6b030da252a7f9
Anton Filippov <a.filippov@protonmail.com>
2018-10-13 18:11:23 +0700

    Fix anonymous field types, indentation

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
         .next()
         .unwrap();
 
-    Ast::new(ron).format(0);
+    Ast::parse_from(ron).format(0, true);
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
@@ -26,10 +26,11 @@ enum Ast {
     NamedTypeUnit(String),
     NamedTypeTuple(String, Vec<Ast>),
     NamedTypeFields(String, BTreeMap<String, Ast>),
+    AnonymousTypeFields(BTreeMap<String, Ast>),
 }
 
 impl Ast {
-    fn new(value: Pair<Rule>) -> Ast {
+    fn parse_from(value: Pair<Rule>) -> Ast {
         let rule = value.as_rule();
         match rule {
             // atomics
@@ -41,7 +42,7 @@ impl Ast {
                 let comma_separated_values = value.into_inner().next().unwrap();
                 let values: Vec<_> = comma_separated_values
                     .into_inner()
-                    .map(|val| Ast::new(val))
+                    .map(|val| Ast::parse_from(val))
                     .collect();
 
                 match rule {
@@ -51,40 +52,56 @@ impl Ast {
                 }
             }
             Rule::map => {
-                let map_inner = value.into_inner().next().unwrap();
-                let entries: BTreeMap<Ast, Ast> = map_inner
-                    .into_inner()
-                    .map(|entry| {
-                        let mut kv_iter = entry.into_inner();
-                        let (key, value) = (kv_iter.next().unwrap(), kv_iter.next().unwrap());
-                        (Ast::new(key), Ast::new(value))
-                    })
-                    .collect();
-                Ast::Map(entries)
+                if let Some(map_inner) = value.into_inner().next() {
+                    let entries: BTreeMap<Ast, Ast> = map_inner
+                        .into_inner()
+                        .map(|entry| {
+                            let mut kv_iter = entry.into_inner();
+                            let (key, value) = (kv_iter.next().unwrap(), kv_iter.next().unwrap());
+                            (Ast::parse_from(key), Ast::parse_from(value))
+                        })
+                        .collect();
+                    Ast::Map(entries)
+                } else {
+                    Ast::Map(Default::default())
+                }
             }
             // named types
             Rule::named_type_unit => Ast::NamedTypeUnit(value.as_str().into()),
             Rule::named_type_tuple => {
                 let mut iter = value.into_inner();
                 let (ident, tuple) = (iter.next().unwrap(), iter.next().unwrap());
-                match Ast::new(tuple) {
+                match Ast::parse_from(tuple) {
                     Ast::Tuple(inner) => Ast::NamedTypeTuple(ident.as_str().into(), inner),
                     _ => unreachable!(),
                 }
             }
             Rule::named_type_fields => {
                 let mut iter = value.into_inner();
-                let (ident, fields) = (iter.next().unwrap(), iter.next().unwrap());
-                let fields: BTreeMap<String, Ast> = fields
+                let (ident, fields_iter) = (iter.next().unwrap(), iter.next().unwrap());
+                let fields: BTreeMap<String, Ast> = fields_iter
                     .into_inner()
                     .map(|field| {
                         let mut field_iter = field.into_inner();
                         let (field_name, field_value) =
                             (field_iter.next().unwrap(), field_iter.next().unwrap());
-                        (field_name.as_str().into(), Ast::new(field_value))
+                        (field_name.as_str().into(), Ast::parse_from(field_value))
                     })
                     .collect();
                 Ast::NamedTypeFields(ident.as_str().into(), fields)
+            }
+            Rule::anonymous_type_fields => {
+                let fields_iter = value.into_inner().next().unwrap();
+                let fields: BTreeMap<String, Ast> = fields_iter
+                    .into_inner()
+                    .map(|field| {
+                        let mut field_iter = field.into_inner();
+                        let (field_name, field_value) =
+                            (field_iter.next().unwrap(), field_iter.next().unwrap());
+                        (field_name.as_str().into(), Ast::parse_from(field_value))
+                    })
+                    .collect();
+                Ast::AnonymousTypeFields(fields)
             }
             Rule::ron_file => {
                 let ron = value.into_inner();
@@ -97,23 +114,27 @@ impl Ast {
                                 extensions.push(ext_name.as_str().into());
                             }
                         }
-                        Rule::value => value = Ast::new(item),
+                        Rule::value => value = Ast::parse_from(item),
                         _ => {}
                     }
                 }
                 Ast::Ron(extensions, Box::new(value))
             }
             // intermediates and aggregates
-            Rule::value | Rule::named_type => Ast::new(value.into_inner().next().unwrap()),
+            Rule::value | Rule::named_type => Ast::parse_from(value.into_inner().next().unwrap()),
             // handled in other rules
             _ => unreachable!(),
         }
     }
 
-    fn format(&self, indent_level: usize) {
+    fn format(&self, indent_level: usize, indent_first_line: bool) {
         fn indent(level: usize) -> String {
             const TAB_SIZE: usize = 4;
             " ".repeat(TAB_SIZE * level)
+        }
+
+        if indent_first_line {
+            print!("{}", indent(indent_level));
         }
 
         match self {
@@ -125,58 +146,69 @@ impl Ast {
                     print!("#![enable({})]\n\n", sorted_exts.join(", "));
                 }
 
-                value.format(0);
+                value.format(0, true);
             }
 
-            Ast::Atom(atom) => print!("{indent}{}", atom, indent = indent(indent_level)),
+            Ast::Atom(atom) => print!("{}", atom),
 
             Ast::Tuple(elements) => {
-                print!("{indent}(\n", indent = indent(indent_level));
+                print!("(\n");
                 for elem in elements {
-                    elem.format(indent_level + 1);
+                    elem.format(indent_level + 1, true);
                     print!(",\n");
                 }
                 print!("{indent})", indent = indent(indent_level));
             },
 
             Ast::List(elements) => {
-                print!("{indent}[\n", indent = indent(indent_level));
+                print!("[\n");
                 for elem in elements {
-                    elem.format(indent_level + 1);
+                    elem.format(indent_level + 1, true);
                     print!(",\n");
                 }
                 print!("{indent}]", indent = indent(indent_level));
             }
 
             Ast::Map(entries) => {
-                print!("{indent}{{\n", indent = indent(indent_level));
+                print!("{{\n");
                 for (key, value) in entries {
-                    key.format(indent_level + 1);
+                    key.format(indent_level + 1, true);
                     print!(": ");
-                    value.format(0);
+                    value.format(indent_level + 1, false);
                     print!(",\n");
                 }
                 print!("{indent}}}", indent = indent(indent_level));
             }
 
-            Ast::NamedTypeUnit(ident) => print!("{indent}{}", ident, indent = indent(indent_level)),
+            Ast::NamedTypeUnit(ident) => print!("{}", ident),
+
             Ast::NamedTypeTuple(ident, elements) => {
-                print!("{indent}{} (\n", ident, indent = indent(indent_level));
+                print!("{}(\n", ident);
                 for elem in elements {
-                    elem.format(indent_level + 1);
+                    elem.format(indent_level + 1, true);
                     print!(",\n");
                 }
                 print!("{indent})", indent = indent(indent_level));
-
             }
+
             Ast::NamedTypeFields(ident, fields) => {
-                print!("{indent}{} {{\n", ident, indent = indent(indent_level));
+                print!("{} (\n", ident);
                 for (field_name, field_value) in fields {
                     print!("{indent}{}: ", field_name, indent = indent(indent_level + 1));
-                    field_value.format(0);
+                    field_value.format(indent_level + 1, false);
                     print!(",\n");
                 }
-                print!("{indent}}}", indent = indent(indent_level));
+                print!("{indent})", indent = indent(indent_level));
+            }
+
+            Ast::AnonymousTypeFields(fields) => {
+                print!("(\n");
+                for (field_name, field_value) in fields {
+                    print!("{indent}{}: ", field_name, indent = indent(indent_level + 1));
+                    field_value.format(indent_level + 1, false);
+                    print!(",\n");
+                }
+                print!("{indent})", indent = indent(indent_level));
             }
         }
     }
