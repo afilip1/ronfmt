f00d722035511a32e95a567ef378599d189283d1
Anton Filippov <a.filippov@protonmail.com>
2018-10-20 12:44:14 +0700

    Initial heuristics-based formatting impl

--- src/ast/display.rs
+++ src/ast/display.rs
@@ -1,8 +1,9 @@
 use super::{Kind, Node};
 use itertools::Itertools;
-use std::fmt::{self, Display, Formatter};
+use std::fmt::{self, Display, Formatter, Write};
 
 const TAB_SIZE: usize = 4;
+const MAX_LINE_WIDTH: usize = 40;
 
 fn indent(level: usize) -> String {
     " ".repeat(TAB_SIZE * level)
@@ -16,11 +17,19 @@ impl Display for Node {
 }
 
 impl Node {
-    fn fmt_rec(&self, f: &mut Formatter, indent_level: usize, indent_first_line: bool) {
+    fn fmt_rec(&self, f: &mut dyn Write, indent_level: usize, indent_first_line: bool) {
         if indent_first_line {
             write!(f, "{}", indent(indent_level));
         }
 
+        if self.0 + TAB_SIZE * indent_level > MAX_LINE_WIDTH {
+            self.fmt_multiline(f, indent_level);
+        } else {
+            write!(f, "{}", self.fmt_singleline());
+        };
+    }
+
+    fn fmt_multiline(&self, f: &mut dyn Write, indent_level: usize) {
         match &self.1 {
             Kind::RonFile(extensions, value) => {
                 if !extensions.is_empty() {
@@ -85,6 +94,63 @@ impl Node {
                 }
 
                 write!(f, "{})", indent(indent_level));
+            }
+        }
+    }
+
+    fn fmt_singleline(&self) -> String {
+        match &self.1 {
+            Kind::RonFile(extensions, value) => {
+                if !extensions.is_empty() {
+                    let mut buf = format!("#![enable({})]\n", extensions.iter().join(", "));
+                    value.fmt_rec(&mut buf, 0, false);
+                    buf
+                } else {
+                    let mut buf = String::new();
+                    value.fmt_rec(&mut buf, 0, false);
+                    buf
+                }
+            }
+
+            Kind::Atom(atom) => atom.clone(),
+
+            Kind::List(elements) => format!("[{}]", elements.iter().join(", ")),
+
+            Kind::Map(entries) => format!(
+                "{{{}}}",
+                entries
+                    .iter()
+                    .map(|(k, v)| format!("{}: {}", k.fmt_singleline(), v.fmt_singleline()))
+                    .join(", ")
+            ),
+
+            Kind::TupleType(ident, elements) => {
+                if let Some(ident) = ident {
+                    format!("{}({})", ident, elements.iter().join(", "))
+                } else {
+                    format!("({})", elements.iter().join(", "))
+                }
+            }
+
+            Kind::FieldsType(ident, fields) => {
+                if let Some(ident) = ident {
+                    format!(
+                        "{}({})",
+                        ident,
+                        fields
+                            .iter()
+                            .map(|(k, v)| format!("{}: {}", k, v.fmt_singleline()))
+                            .join(", ")
+                    )
+                } else {
+                    format!(
+                        "({})",
+                        fields
+                            .iter()
+                            .map(|(k, v)| format!("{}: {}", k, v.fmt_singleline()))
+                            .join(", ")
+                    )
+                }
             }
         }
     }
